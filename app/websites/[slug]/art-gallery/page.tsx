c442f6c41efe04a3ead2f38d3562c189138ac4a9
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2025-12-10 21:48:09 +0300

    s

--- app/websites/[slug]/art-gallery/page.tsx
+++
@@ -1,6 +0,0 @@
-// app/websites/art-gallery/page.tsx
-import ArtGalleryTemplate from '@/lib/templates/ArtGalleryTemplate';
-
-export default function ArtGalleryPage() {
-  return <ArtGalleryTemplate />;
-}
