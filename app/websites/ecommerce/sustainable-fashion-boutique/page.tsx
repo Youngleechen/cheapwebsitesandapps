00666c262d7ad58f0a4a39fb4cae2291565726c9
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2025-12-12 18:23:25 +0300

    d

--- app/websites/ecommerce/sustainable-fashion-boutique/page.tsx
+++ app/websites/ecommerce/sustainable-fashion-boutique/page.tsx
@@ -1009,4 +1009,4 @@ const ChevronDown = ({ className = 'w-6 h-6' }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className={className}>
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
   </svg>
-);G
+);
